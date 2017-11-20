e42cb242fcd25421a571f4a5fc143082873dc7c1
Todd Oh <toddoh@Todds-MacBook-Pro.local>
2017-11-20 22:11:42 +0900

    Fixed cron configuration param

--- kevin/manage_crons.py
+++ kevin/manage_crons.py
@@ -10,8 +10,7 @@ for job in cron:
 print('Adding a new cron tasks: ')
 job = cron.new(command='cd /var/www/kevin/kevin/ && /var/envs/kevin/bin/python action_aggregation.py --target=trumptimemachine',
                comment='minion_trumptimemachine_aggregator')
-job.minute().on(0)
-job.hour().on(6)
+job.hour.every(12)
 
 job.enable()
 cron.write()
