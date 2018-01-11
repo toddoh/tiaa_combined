f197b3f926927a63ff4152350bfa7581c124ee26
Todd Oh <toddoh@Todds-MacBook-Pro.local>
2018-01-11 15:52:45 +0900

    Fixed middleware header

--- server.js
+++ server.js
@@ -15,6 +15,24 @@ app.listen(3000, function () {
     console.log('Example app listening on port 3000!\n');
 });
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(express.static(path.join(__dirname, '/dist/')));
 
 app.get('/dataset/:id/:type', function (request, response) {
