632becf31482cff0eecdffc598cf971a98b7022d
Todd Oh <toddoh@Todds-MacBook-Pro.local>
2018-01-21 15:05:46 +0900

    Fixed content detail popup scrolling issue

--- src/js/about.js
+++ src/js/about.js
@@ -13,7 +13,8 @@ export function init_render() {
                 <p>It's getting more important to be aware of what's happening to support democracy and society. However, in the era of digital journalism, it is particularly difficult to know what are the things which matter. With a clear and transparent data-driven effort, we strive to highlight every aspects of society and things that can impact our daily lives. We, THISISALLABOUT, believe that data-driven effort can restore damaged journalism these days.</p>
             </div>
             <div class="hero2">
-                <p>- We use a sophisticated clustering/natural language processing algorithm to provide our contents. Check the information attached to every section to find out details.</p>
+                <p>Editorial director: Todd Oh</p>
+                <p>* We use a sophisticated clustering/natural language processing algorithm to provide our contents. Check the information attached to every section to find out details.</p>
             </div>
             <div class="hero2">
                 <p><a href="mailto:hello@thisisallabout.com">Contact Us</a></p>
