431550f680250eb9e2d01af558fe847dc9c6bf7c
Todd Oh <toddoh@Todds-MacBook-Pro.local>
2018-01-31 15:49:54 +0900

    Updated python dependencies

--- kevin/eastasia/unit_policycluster.py
+++
@@ -1,7 +0,0 @@
-from eastasia.parser_articles_ko import parse_aggregated
-
-with open('./aggregated.txt') as data_file:
-    result_data = data_file.readlines()
-
-parsed_data = parse_aggregated(result_data, 1, 15)
-print(parsed_data)
